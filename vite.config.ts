11489cd4f03d1a83ef92a5ab6e7eb4e6c0ad531a
fpesantez <54825485+pixelknit@users.noreply.github.com>
2024-12-11 12:41:49 +0000

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import restart from 'vite-plugin-restart'
+
+export default defineConfig({
+    root: 'src/',
+    publicDir: '../static/',
+    server: {
+        host: true,
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true
+    },
+    plugins: [
+        restart({ restart: ['../static/**'] })
+    ]
+})
